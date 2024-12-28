21476d35f13b524c36fa3b6c0512c60a840c5740
pranavsachu <pranavsachu2005@gmail.com>
2024-12-28 21:32:04 +0530

    Added some of the pages

--- Backend/HVH/app/views.py
+++ Backend/HVH/app/views.py
@@ -5,8 +5,16 @@ def index(request):
     return render(request, 'index.html')
 
 def home(request):
-    # return render(request, 'index.html')
-    return JsonResponse({"success": "success"}, status=200)
+    return render(request, 'home.html')
 
 def login(request):
     return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def heatmap(request):
+    return render(request, 'heatmap.html')
+
+def forgot(request):
+    return render(request, 'forgot.html')
